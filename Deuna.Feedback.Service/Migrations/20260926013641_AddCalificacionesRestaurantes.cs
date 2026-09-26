db3b93fd0ae2d2c049ff21416b76aef7b82547e2
Andres David <andres.david@deuna.com>
2026-09-25 20:49:34 -0500

    feat(feedback): migrar las columnas de calificaciones por restaurante

---
+++ Deuna.Feedback.Service/Migrations/20260926013641_AddCalificacionesRestaurantes.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Deuna.Feedback.Service.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCalificacionesRestaurantes : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Ciudad",
+                table: "restaurantes_replicados",
+                type: "character varying(100)",
+                maxLength: 100,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Ciudad",
+                table: "restaurantes_replicados");
+        }
+    }
+}
